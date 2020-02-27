8ac17537f709d16d741bb93dc644f18b051aa579
Callum Stott <callum@seadowg.com>
2020-02-27 17:30:41 +0000

    Pull out add repeat analytics event as constant

---
+++ collect_app/src/main/java/org/odk/collect/android/analytics/AnalyticsEvents.java
@@ -0,0 +1,14 @@
+package org.odk.collect.android.analytics;
+
+public class AnalyticsEvents {
+
+    private AnalyticsEvents() {
+
+    }
+
+    /**
+     * Used to measure how popular different methods of adding repeats are. The "action" should
+     * describe the way the repeat was added.
+     */
+    public static final String ADD_REPEAT = "AddRepeat";
+}
